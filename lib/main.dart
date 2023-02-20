b40ab184c168aa11899b6abd4fdf5abcbcadb20f
Mohammed Adel <mohammedadel111111@gmail.com>
2023-02-20 22:50:13 +0200

    create login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/ui/splash_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
+      designSize:  Size(375, 812),
       builder: (context, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
@@ -24,6 +24,6 @@ class MyApp extends StatelessWidget {
           home: SplashScreen(),
         );
       },
-    );
+    ); 
   }
 }
